5c04204a0725a27170f15805951ca552598bbdfb
zhong <zhongwen@msu.edu>
2021-05-18 01:25:17 -0400

    Initial commit

---
+++ demo/admin.py
@@ -0,0 +1,200 @@
+import datetime
+
+from django.contrib import admin, messages
+from django.db import transaction
+from django.http import JsonResponse
+from django.urls import reverse
+
+from simpleui.admin import AjaxAdmin
+from .models import *
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin, ImportExportActionModelAdmin
+
+
+# Register your models here.
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    # 要显示的字段
+    list_display = ('id', 'name', 'create_time')
+
+    # 需要搜索的字段
+    search_fields = ('name',)
+
+    # 分页显示，一页的数量
+    list_per_page = 10
+
+    actions_on_top = True
+
+
+class ImageInline(admin.TabularInline):
+    model = Image
+
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    # 要显示的字段
+    list_display = ('id', 'name')
+
+    # 需要搜索的字段
+    search_fields = ('name',)
+
+    # 分页显示，一页的数量
+    list_per_page = 10
+    inlines = [
+        ImageInline,
+    ]
+
+
+class AgeListFilter(admin.SimpleListFilter):
+    title = u'最近生日'
+    parameter_name = 'ages'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('0', u'最近7天'),
+            ('1', u'最近15天'),
+            ('2', u'最近30天'),
+        )
+
+    def queryset(self, request, queryset):
+        # 当前日期格式
+        cur_date = datetime.datetime.now().date()
+
+        if self.value() == '0':
+            # 前一天日期
+            day = cur_date - datetime.timedelta(days=1)
+
+            return queryset.filter(birthday__gte=day)
+        if self.value() == '1':
+            day = cur_date - datetime.timedelta(days=15)
+            return queryset.filter(birthday__gte=day)
+        if self.value() == '2':
+            day = cur_date - datetime.timedelta(days=30)
+            return queryset.filter(birthday__gte=day)
+
+
+class ProxyResource(resources.ModelResource):
+    class Meta:
+        model = Employe
+
+
+@admin.register(Employe)
+class EmployeAdmin(ImportExportActionModelAdmin,AjaxAdmin):
+    resource_class = ProxyResource
+    list_display = ('id', 'name', 'gender', 'phone', 'birthday', 'department', 'enable', 'create_time')
+    # search_fields = ('name', 'enable', 'idCard', 'department')
+    search_fields = ('name', 'department__name')
+    list_per_page = 20
+    raw_id_fields = ('department', 'title')
+    list_filter = ('department', AgeListFilter, 'create_time')
+    # list_filter = (AgeListFilter, 'department', 'create_time', 'birthday', 'time', 'enable', 'gender')
+
+    list_display_links = ('name',)
+
+    list_editable = ('department', 'phone', 'birthday', 'enable', 'gender')
+
+    date_hierarchy = 'create_time'
+
+    fieldsets = [(None, {'fields': ['name', 'gender', 'phone']}),
+                 (u'其他信息', {
+                     'classes': ('123',),
+                     'fields': ['birthday', 'department', 'enable']})]
+    save_on_top = True
+    @transaction.atomic
+    def test(self, request, queryset):
+        messages.add_message(request, messages.SUCCESS, '啥也没有~')
+        pass
+
+    # 自 3.4+ 支持confirm确认提示
+    test.confirm = '您确定要点击测试按钮吗？'
+
+    # 增加自定义按钮
+    actions = [test, 'make_copy', 'custom_button','layer_input']
+
+    def layer_input(self, request, queryset):
+        # 这里的queryset 会有数据过滤，只包含选中的数据
+
+        post = request.POST
+        # 这里获取到数据后，可以做些业务处理
+        # post中的_action 是方法名
+        # post中 _selected 是选中的数据，逗号分割
+
+        return JsonResponse(data={
+            'status': 'success',
+            'msg': '处理成功！'
+        })
+
+    layer_input.short_description = '弹出对话框输入'
+    layer_input.type = 'success'
+    layer_input.icon = 'el-icon-s-promotion'
+
+    # 指定一个输入参数，应该是一个数组
+
+    # 指定为弹出层，这个参数最关键
+    layer_input.layer = {
+        # 弹出层中的输入框配置
+
+        # 这里指定对话框的标题
+        'title': '弹出层输入框',
+        # 提示信息
+        'tips': '这个弹出对话框是需要在admin中进行定义，数据新增编辑等功能，需要自己来实现。',
+        # 确认按钮显示文本
+        'confirm_button': '确认提交',
+        # 取消按钮显示文本
+        'cancel_button': '取消',
+
+        # 弹出层对话框的宽度，默认50%
+        'width': '40%',
+
+        # 表单中 label的宽度，对应element-ui的 label-width，默认80px
+        'labelWidth': "80px",
+        'params': [{
+            # 这里的type 对应el-input的原生input属性，默认为input
+            'type': 'input',
+            # key 对应post参数中的key
+            'key': 'name',
+            # 显示的文本
+            'label': '名称',
+            # 为空校验，默认为False
+            'require': True
+        }]
+    }
+
+    def custom_button(self, request, queryset):
+        pass
+
+    # 显示的文本，与django admin一致
+    custom_button.short_description = '测试按钮'
+    # icon，参考element-ui icon与https://fontawesome.com
+    custom_button.icon = 'fas fa-audio-description'
+
+    # 指定element-ui的按钮类型，参考https://element.eleme.cn/#/zh-CN/component/button
+    custom_button.type = 'danger'
+
+    # 给按钮追加自定义的颜色
+    custom_button.style = 'color:black;'
+
+    # 链接按钮，设置之后直接访问该链接
+    # 3中打开方式
+    # action_type 0=当前页内打开，1=新tab打开，2=浏览器tab打开
+    # 设置了action_type，不设置url，页面内将报错
+
+    custom_button.action_type = 1
+    custom_button.action_url = 'https://www.baidu.com'
+
+    def make_copy(self, request, queryset):
+        ids = request.POST.getlist('_selected_action')
+        for id in ids:
+            employe = Employe.objects.get(id=id)
+
+            Employe.objects.create(
+                name=employe.name,
+                idCard=employe.idCard,
+                phone=employe.phone,
+                birthday=employe.birthday,
+                department_id=employe.department_id
+            )
+
+        messages.add_message(request, messages.SUCCESS, '复制成功，复制了{}个员工。'.format(len(ids)))
+
+    make_copy.short_description = '复制员工'
